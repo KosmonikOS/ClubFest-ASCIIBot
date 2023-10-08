645815fb648a0e65d1bf384618fe757633e85cc4
Nikita Tyurkov <nik.lizard.mobile@gmail.com>
2023-10-08 15:03:44 +0300

    Eliminating output size parameter

--- ClubFestBotCSharp/Messages.cs
+++ ClubFestBotCSharp/Messages.cs
@@ -6,7 +6,7 @@
         "To convert your photo send it with font size in a caption \n" +
         "Note that font size should be positive integer.";
         public const string InvalidArgumentsMessage = "Invalid input values";
-        public const string TooLargeInputMessage = "Try another font size or resolution";
+        public const string TooLargeInputMessage = "Try another font size";
     }
 
 }
