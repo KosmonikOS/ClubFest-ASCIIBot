dc29f7401c57a452c4463e055e6bbee6ddaf99b1
Nikita Tiurkov <86474529+KosmonikOS@users.noreply.github.com>
2023-10-09 19:34:56 +0300

    Deleting API key

--- ClubFestBotCSharp/Program.cs
+++ ClubFestBotCSharp/Program.cs
@@ -4,7 +4,7 @@ using System.Text;
 //Register encoding
 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-var bot = new ASCIIConvertTelegramBot("6363511412:AAEvE7XPKt4aYVZnMGApLhEN0lRJ37YE-v4");
+var bot = new ASCIIConvertTelegramBot("API key");
 
 using CancellationTokenSource cts = new();
 
